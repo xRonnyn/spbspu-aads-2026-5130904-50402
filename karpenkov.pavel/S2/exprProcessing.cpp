201167ff36f62fd7185f9bb9877eff98347da279
xRonnyn <pashuha.k@gmail.com>
2026-04-03 23:29:15 +0300

    inf to postfix without op priority

--- karpenkov.pavel/S2/exprProcessing.cpp
+++ karpenkov.pavel/S2/exprProcessing.cpp
@@ -1,8 +1,65 @@
+#include <cctype>
 #include <iostream>
 #include "queue.hpp"
 #include "stack.hpp"
 
 using queueExpr = karpenkov::Queue< std::string >;
+
+bool isNumber(std::string &element)
+{
+  return std::isdigit(element[0]);
+}
+
+bool isValidOperator(std::string &element)
+{
+  if (element == "+" || element == "-" || element == "*" || element == "/") {
+    return true;
+  }
+  return false;
+}
+size_t priority(std::string &operation)
+{
+  if (operation == "+" || operation == "-") {
+    return 1;
+  } else if (operation == "/" || operation == "*") {
+    return 2;
+  }
+}
+
+karpenkov::Stack< queueExpr > toPostfix(karpenkov::Stack< queueExpr > expressions)
+{
+  karpenkov::Stack< queueExpr > postFix;
+  while (!expressions.empty()) {
+    karpenkov::Queue< std::string > resultExpr;
+    karpenkov::Stack< std::string > tempStep;
+    karpenkov::Queue< std::string > curExpr = expressions.top();
+    while (!curExpr.empty()) {
+      size_t pr;
+      std::string element = curExpr.front();
+      if (element == "(") {
+        tempStep.push(element);
+      } else if (element == ")") {
+        while (!tempStep.empty()) {
+          if (tempStep.top() != "(") {
+            while (pr < priority(tempStep.top())) {
+              resultExpr.push(tempStep.top());
+              pr = priority(tempStep.top());
+            }
+          }
+          tempStep.pop();
+        }
+      } else if (isNumber(element)) {
+        resultExpr.push(element);
+      } else if (isValidOperator(element)) {
+        pr = priority(element);
+        tempStep.push(element);
+      }
+      curExpr.pop();
+    }
+    postFix.push(resultExpr);
+  }
+  return postFix;
+}
 
 karpenkov::Stack< queueExpr > inputCLI(std::istream &in)
 {
