9220934302c93bda2cd3466a57bbec1c7827856b
xRonnyn <pashuha.k@gmail.com>
2026-06-10 18:58:15 +0300

    hashTable add()

--- karpenkov.pavel/S3/hashTable.hpp
+++ karpenkov.pavel/S3/hashTable.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #ifndef HASHTABLE_HPP
 #define HASHTABLE_HPP
 enum State { EMPTY, OCCUPIED, TOMBSTONE };
@@ -7,6 +8,24 @@ public:
   HashTable(Hash hash, Equal equal)
       : size_(0), capacity_(10), hash_(hash), equal_(equal) {
     table_ = new Unit[capacity_];
+  }
+  void add(Key k, Value v) {
+    size_t index = hash_(k) % capacity_;
+    for (size_t i = 0; i < capacity_; i++) {
+      size_t cur = (index + cur) % capacity_;
+      if (table_[cur].state == EMPTY || table_[cur].state == TOMBSTONE) {
+        table_[cur].key = k;
+        table_[cur].value = v;
+        table_[cur].state = OCCUPIED;
+        size_++;
+        return;
+      }
+      if (table_[cur].state == OCCUPIED && equal_(table_[cur].key, k)) {
+        table_[cur].key = k;
+        return;
+      }
+    }
+    throw std::runtime_error("HashTable is full");
   }
 
 private:
