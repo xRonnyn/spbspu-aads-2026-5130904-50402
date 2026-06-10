e01aefe7ea7ef3aca8acf68548b328d5f70f562d
xRonnyn <pashuha.k@gmail.com>
2026-06-10 20:03:09 +0300

    rehash()

--- karpenkov.pavel/S3/hashTable.hpp
+++ karpenkov.pavel/S3/hashTable.hpp
@@ -39,6 +39,37 @@ public:
     }
     return false;
   }
+  Value drop(Key k) {
+    size_t index = hash_(k) % capacity_;
+    for (size_t i = 0; i < capacity_; ++i) {
+      size_t cur = (index + 1) % capacity_;
+      if (table_[cur].state == OCCUPIED && equal_(table_[cur].key, k)) {
+        table_[cur].state = TOMBSTONE;
+        --size_;
+        return table_[cur].value;
+      }
+      if (table_[cur].state == EMPTY) {
+        throw std::runtime_error("key not found");
+      }
+    }
+    throw std::runtime_error("key not found");
+  }
+  void rehash(size_t slots) {
+    Unit *old_table = table_;
+    size_t old_capacity = capacity_;
+
+    capacity_ = slots;
+    table_ = new Unit[capacity_];
+    size_ = 0;
+
+    for (size_t i = 0; i < old_capacity; ++i) {
+      if (old_table[i].state == OCCUPIED) {
+        add(old_table[i].key, old_table[i].value);
+      }
+    }
+
+    delete[] old_table;
+  }
 
 private:
   struct Unit {
