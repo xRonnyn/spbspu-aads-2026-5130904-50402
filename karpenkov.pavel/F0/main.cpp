07bf3525b9e1bc56c86263a64154c1e54af0ffc0
xRonnyn <pashuha.k@gmail.com>
2026-06-22 00:23:29 +0300

    test main

--- karpenkov.pavel/F0/main.cpp
+++ karpenkov.pavel/F0/main.cpp
@@ -1,2 +1,19 @@
+#include "graph.hpp"
 #include <iostream>
-int main() {}
+using namespace karpenkov;
+int main() {
+  Graph graph;
+  graph.insertMovie("A", "B");
+  graph.insertMovie("B", "C");
+  graph.insertMovie("D", "C");
+  std::cout << "Order:\n";
+  graph.showOrder();
+  std::cout << "Dependencies of B:\n";
+  graph.showDependencies("B");
+  std::cout << '\n';
+  std::cout << "Successors of C:\n";
+  graph.showSuccessors("C");
+  std::cout << '\n';
+  graph.findCycles();
+  return 0;
+}
